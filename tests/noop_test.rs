38b1958a43a2b769e579935b3992174fe72e9661
Kevin Sullivan <kevin@sull.vn>
2023-03-13 11:49:37 -0500

    Cleanup test documentation

--- tests/noop_test.rs
+++ tests/noop_test.rs
@@ -1,3 +1,14 @@
+//!
+//! Integration tests
+//!
+//! ### Todo
+//!
+//! - Mock out OpenAI API service
+//!   - Test match with real service once a day
+//!   - Drop-in replacement for ureq-implementation
+//!   - Assert parts of API request
+//!   - Provide response
+//!
 use std::env::{self, current_dir};
 use std::fs::File;
 use std::path::PathBuf;
@@ -6,16 +17,6 @@ use tempfile::tempdir;
 
 use shai::{Result, CONFIG_DIR_NAME, CONFIG_FILE_NAME, HOME_CONFIG_DIR, OPENAI_API_KEY_ENV_KEY};
 
-// Setup helpers:
-//
-// - Get path of built binary
-//   - Hardcode
-// - Set environment variables
-//   - Command::env
-// - Get output
-//   - Command::output
-// - Create temporary directory
-//
 fn shai_command() -> Result<Command> {
     let binary_path: PathBuf = [current_dir()?, "target/debug/shai".into()]
         .iter()
@@ -23,11 +24,6 @@ fn shai_command() -> Result<Command> {
     Ok(Command::new(binary_path))
 }
 
-// Integration tests:
-//
-// Now:
-//
-// - Get failure on no api key setup
 #[test]
 fn no_api_key() -> Result<()> {
     let home_dir = tempdir()?;
@@ -44,9 +40,6 @@ fn no_api_key() -> Result<()> {
     Ok(())
 }
 
-// - API key via environment variable
-//   - Call "command" with API key in environment variable
-//   - Confirm valid output
 #[test]
 fn api_key_environment_variable() -> Result<()> {
     let api_key = env::var(OPENAI_API_KEY_ENV_KEY)?;
@@ -67,15 +60,6 @@ fn api_key_environment_variable() -> Result<()> {
     Ok(())
 }
 
-// - API key via config file
-//   - Set $HOME
-//   - Call "configure" with api key
-//   - Confirm the config file location
-//   - Call "command"
-//   - Confirm valid output
-//   - Set $HOME to bad directory, set $XDG_CONFIG_HOME
-//   - Call "command"
-//   - Confirm valid output
 #[test]
 fn api_key_config_file() -> Result<()> {
     let api_key = env::var(OPENAI_API_KEY_ENV_KEY)?;
@@ -108,13 +92,7 @@ fn api_key_config_file() -> Result<()> {
         .env("HOME", home_dir.path())
         .output()?;
 
-    assert!(
-        !command_output.stdout.is_empty(),
-        "stdout='{:?}' stderr='{:?}' status='{:?}'",
-        command_output.stdout,
-        command_output.stderr,
-        command_output.status.code(),
-    );
+    assert!(!command_output.stdout.is_empty());
     assert!(command_output.stderr.is_empty());
     assert!(command_output.status.success());
 
@@ -132,13 +110,3 @@ fn api_key_config_file() -> Result<()> {
 
     Ok(())
 }
-//
-// Later:
-//
-// - Mock out OpenAI API service
-//   - Test match with real service once a day
-//   - Drop-in replacement for ureq-implementation
-//   - Assert parts of API request
-//   - Provide response
-//
-//
